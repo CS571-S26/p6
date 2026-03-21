72e9a223d3aa0f6e11a017278a0e5dfd57b5320c
Quinton <qbush@wisc.edu>
2026-03-21 08:17:27 -0500

    Adding bike shop widget

--- src/pages/ShopBikes.jsx
+++ src/pages/ShopBikes.jsx
@@ -1,8 +1,39 @@
-import { Container, Button } from 'react-bootstrap'
+import { useEffect } from 'react'
+import { Container } from 'react-bootstrap'
 import '../styles/ShopBikes.css'
-import shopImg from '../assets/fullInterior.webp'
 
 export default function ShopBikes() {
+  useEffect(() => {
+    const config = {
+      store: '321934',
+      uri: 'search',
+    }
+
+    const query = Object.keys(config)
+      .map(
+        (k) => encodeURIComponent(k) + '=' + encodeURIComponent(config[k])
+      )
+      .join('&')
+
+    const endpoint =
+      'https://oregonbicyclecompany.locally.com/widgets/search.js?' + query
+
+    // Remove any previous script to avoid duplicates
+    const existing = document.getElementById('lcly-script-0')
+    if (existing) existing.remove()
+
+    const script = document.createElement('script')
+    script.id = 'lcly-script-0'
+    script.src = endpoint
+    script.async = true
+    document.getElementById('lcly-button-0').appendChild(script)
+
+    return () => {
+      const el = document.getElementById('lcly-script-0')
+      if (el) el.remove()
+    }
+  }, [])
+
   return (
     <>
       <Container className="py-5 text-center">
@@ -10,24 +41,28 @@ export default function ShopBikes() {
         <p className="shop-bikes-subtitle">
           Browse our full selection of bikes available in store.
         </p>
-        <Button
-          href="https://oregonbicyclecompany.locally.com/search?store=321934"
-          target="_blank"
-          rel="noopener noreferrer"
-          variant="outline-primary"
-          size="lg"
-          className="shop-bikes-btn mt-3"
-        >
-          View Live Inventory
-        </Button>
       </Container>
 
-      <div className="shop-bikes-banner">
-        <img
-          src={shopImg}
-          alt="Inside Oregon Bicycle Company"
-          className="shop-bikes-banner-img"
-        />
+      <div className="shop-bikes-widget-wrapper">
+        <div
+          id="lcly-button-0"
+          data-switchlive="true"
+          data-switchlive-impression="true"
+          data-switchlive-impression-id-pl="1"
+        >
+          <a
+            id="lcly-link-0"
+            data-switchlive="true"
+            data-switchlive-mode="auto"
+            data-switchlive-id-pl="6"
+            role="button"
+            href="https://www.locally.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Powered by Locally
+          </a>
+        </div>
       </div>
     </>
   )

34f478e499acdc09327adef4b2187f6bae0a7297
Quinton <qbush@wisc.edu>
2026-04-21 14:57:06 -0500

    Adding back /p6/ to base for github pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 // Note will probably have to change base back to /p6/ for the project
 export default defineConfig({
-  base: '/',
+  base: '/p6/',
   plugins: [react()],
   build: {
     outDir: "docs"
